aca12a61ab94a4d4196cab23c23e9750caf7f64a
Mohamed ElKalioby <melkalioby@kfshrc.edu.sa>
2018-04-22 15:40:46 +0300

    Allowing Kwargs Style

--- jsonLookup/has_lookup.py
+++ jsonLookup/has_lookup.py
@@ -10,7 +10,7 @@ class hasLookup(Lookup):
         rhs, rhs_params = self.process_rhs(compiler, connection)
 
         params = lhs_params + rhs_params
-        lst=rhs_params[0].split("=")
+        lst=rhs_params[0].replace("'","").replace('"','').split("=")
         lst[0]=lst[0].strip()
         lst[1]=lst[1].strip()
         if lst[1].isdigit():
--- jsonLookup/shas_lookup.py
+++ jsonLookup/shas_lookup.py
@@ -10,7 +10,7 @@ class shasLookup(Lookup):
         rhs, rhs_params = self.process_rhs(compiler, connection)
 
         params = lhs_params + rhs_params
-        lst=rhs_params[0].split("=")
+        lst=rhs_params[0].replace("'","").replace('"','').split("=")
         lst[0]=lst[0].strip()
         lst[1]=lst[1].strip()
         return "JSON_EXTRACT(%s,'%s')= %s" %(lhs, lst[0],rhs),[lst[1]]
