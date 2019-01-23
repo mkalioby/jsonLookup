9df843d9f9d1d436a826b463dc33400a4b8bc467
Mohamed ElKalioby <melkalioby@kfshrc.edu.sa>
2019-01-23 10:40:10 +0300

    Fixes with Py3

--- jsonLookup/__init__.py
+++ jsonLookup/__init__.py
@@ -1,3 +1,4 @@
-from has_lookup import *
-from jcontains_lookup import *
-from shas_lookup import *
+from .has_lookup import *
+from .jcontains_lookup import *
+from .shas_lookup import *
+from .jlength_lookup import *
--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
-
+import setuptools
 from distutils.core import setup
 
 setup(name='jsonLookup',
-      version='0.4.0',
+      version='0.5.2',
       description='Search MySQL JSON fields in Django',
       author='Mohamed El-Kalioby',
       author_email='mkalioby@mkalioby.com',
       url='https://github.com/mkalioby/jsonLookup',
       packages=['jsonLookup'],
+      long_description=open("README.md").read(),
+      long_description_content_type="text/markdown",
       keywords = ['django','django', 'mysql'],
      )
