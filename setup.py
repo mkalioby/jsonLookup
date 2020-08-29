8b72e7527e126ae7715372b76b89dd3826d369c7
Mohamed ElKalioby <mkalioby@mkalioby.com>
2020-08-29 19:34:23 +0300

    Added License file to package, closes #1

--- setup.py
+++ setup.py
@@ -3,12 +3,13 @@ import setuptools
 from distutils.core import setup
 
 setup(name='jsonLookup',
-      version='0.7.5',
+      version='0.8.0',
       description='Search MySQL JSON fields in Django',
       author='Mohamed El-Kalioby',
       author_email='mkalioby@mkalioby.com',
       url='https://github.com/mkalioby/jsonLookup',
       packages=['jsonLookup'],
+      data_files=[("", ["LICENSE"])],
       long_description=open("README.md").read(),
       long_description_content_type="text/markdown",
       keywords = ['django','django', 'mysql'],
